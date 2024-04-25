159a16917347d91d03e3934fd2bf45c679460d62
Firoz Mahmud Rizvi <firoz.rizvi@brainstation-23.com>
2024-04-25 12:30:13 +0600

    Rebase + Added iterative fibonacci from main branch + resolve conflict

--- basic_program.cpp
+++ basic_program.cpp
@@ -17,9 +17,18 @@ int calculateFactorial(int n){
     return result;
 }
 
-//Added from experimental-branch
+//Added from main
 int calculateFibonacci(int n){
-    if (n <= 1) 
-        return n; 
-    return fib(n - 1) + fib(n - 2);
+    int f[n + 2];  
+    int i; 
+    f[0] = 0; 
+    f[1] = 1; 
+  
+    for(i = 2; i <= n; i++) 
+    { 
+
+       f[i] = f[i - 1] + f[i - 2]; 
+    } 
+    return f[n]; 
+    } 
 }
