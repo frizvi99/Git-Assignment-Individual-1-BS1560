aaf7e1da945392cdd57291606da94e9075e42793
Firoz Mahmud Rizvi <firoz.rizvi@brainstation-23.com>
2024-04-25 13:15:51 +0600

    Added a function to find nth prime number

--- basic_program.cpp
+++ basic_program.cpp
@@ -14,3 +14,26 @@ int calculateFactorial(int n){
 
     return result;
 }
+
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+    if (num % 2 == 0 || num % 3 == 0) return false;
+    for (int i = 5; i * i <= num; i += 6) {
+        if (num % i == 0 || num % (i + 2) == 0) return false;
+    }
+    return true;
+}
+
+int nthPrime(int n) {
+    if (n <= 0) return -1; 
+    std::vector<int> primes;
+    int num = 2; 
+    while (primes.size() < n) {
+        if (isPrime(num)) {
+            primes.push_back(num);
+        }
+        num++;
+    }
+    return primes.back(); 
+}
